810e28be0066ee245de02fe55a7d4bf1530f66ec
José Rodrigues <rjosefdev@gmail.com>
2026-05-30 15:19:24 -0300

    exercicio 2: endpoints put patch delete

--- src/main/java/com/Jose/AulaPos/Controllers/PessoaController.java
+++ src/main/java/com/Jose/AulaPos/Controllers/PessoaController.java
@@ -1,8 +1,10 @@
 package com.Jose.AulaPos.Controllers;
 
 import com.Jose.AulaPos.Models.Pessoa;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,5 +34,58 @@ public class PessoaController {
         return pessoas.stream()
                 .filter(i -> i.getNome().equalsIgnoreCase(nome))
                 .findFirst();
+    }
+
+    @PutMapping("/pessoa/{id}")
+    @ResponseBody
+    public Pessoa substituirPessoa(@PathVariable Long id, @RequestBody Pessoa novaPessoa) {
+        Pessoa pessoa = pessoas.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Pessoa não encontrada."
+                ));
+
+        pessoa.setId(novaPessoa.getId());
+        pessoa.setNome(novaPessoa.getNome());
+
+        return pessoa;
+    }
+
+    @PatchMapping("/pessoa/{id}")
+    @ResponseBody
+    public Pessoa atualizarPessoa(@PathVariable Long id, @RequestBody Pessoa dadosAtualizacao) {
+        Pessoa pessoa = pessoas.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Pessoa não encontrada."
+                ));
+
+        if (dadosAtualizacao.getNome() != null) {
+            pessoa.setNome(dadosAtualizacao.getNome());
+        }
+
+        if (dadosAtualizacao.getId() != null) {
+            pessoa.setId(dadosAtualizacao.getId());
+        }
+
+        return pessoa;
+    }
+
+    @DeleteMapping("/pessoa/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarPessoa(@PathVariable Long id) {
+        Pessoa pessoa = pessoas.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Pessoa não encontrada."
+                ));
+
+        pessoas.remove(pessoa);
     }
 }
